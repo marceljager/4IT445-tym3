6e60e14e35a437b61e703d2ed831858a6eea153b
Marcel Jäger <mjager9@gmail.com>
2017-10-21 16:38:59 +0200

    user profile page added

--- frontend/src/pages/PageContent.js
+++ frontend/src/pages/PageContent.js
@@ -4,12 +4,14 @@ import { Route } from 'react-router-dom';
 import { Login } from './Login';
 import { Home } from './Home';
 import SearchPage from './SearchPage';
+import UserProfile from './UserProfile';
 
 const PageContent = () => (
     <div className="PageContent">
         <Route exact path="/" component={Login} />
         <Route path="/timeline" component={Home} />
         <Route path="/hledat/:searchText" component={SearchPage} />
+        <Route path="/uzivatel/:userId" component={UserProfile} />
     </div>
 );
 
---
+++ frontend/src/pages/UserProfile.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const UserProfile = () => (
+    <div>
+        <h1>User profile</h1>
+    </div>
+);
+
+export default UserProfile;
