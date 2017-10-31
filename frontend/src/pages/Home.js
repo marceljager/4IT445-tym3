a06919ffe186f57933fd5fa9a92798fee0f4ac75
romanfausek <romanfausek@gmail.com>
2017-10-31 21:27:35 +0100

    bootstrap, typo, general styles, buttons, header

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -8,11 +8,39 @@ export class HomeRaw extends Component {
 
         return (
             <div>
-                <h1>Home</h1>
-                {user.email &&
-                <div>Přihlášen: {user.firstName} {user.lastName}</div>
-                }
-                <Link to="/">Login</Link>
+                {/*<h1>Home</h1>*/}
+                {/*{user.email &&*/}
+                {/*<div>Přihlášen: {user.firstName} {user.lastName}</div>*/}
+                {/*}*/}
+                {/*<Link to="/">Login</Link>*/}
+
+                <header className="Header">
+                    <div className="Header-overlay"></div>
+                    <div className="container">
+                        <div className="Header-content">
+                            <h1>Najez se v příjemné společnosti</h1>
+                            <p>Pozvi přátele na oběd nebo se připoj k jedné z více než 120 akcí v okolí.</p>
+
+                            <Link to="/" className="Button Button--big">
+                                <span className="Button-text">
+                                    objev akce v okolí
+                                </span>
+                            </Link>
+
+                            <div>
+                                <p>
+                                    nebo <Link to="/" className="Header-link">vytvoř novou akci</Link>
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </header>
+
+                <Link to="/" className="Button Button--transparent">
+                    <span className="Button-text">
+                        Přihlásit se
+                    </span>
+                </Link>
             </div>
         );
     }
